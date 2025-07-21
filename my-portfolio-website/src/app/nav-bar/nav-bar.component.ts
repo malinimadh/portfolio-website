9b70c0e2acaa50bdff2b53d62da5b47fe11c71b4
malinimadh <156970520+malinimadh@users.noreply.github.com>
2025-07-21 18:19:01 -0400

    icon changes

--- my-portfolio-website/src/app/nav-bar/nav-bar.component.ts
+++ my-portfolio-website/src/app/nav-bar/nav-bar.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import {MatIconModule} from '@angular/material/icon';
 
 @Component({
   selector: 'app-nav-bar',
   standalone: true,
-  imports: [],
+  imports: [MatIconModule],
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.css'
 })
